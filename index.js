9372f3efae4ffed478f9eec30a94cc235f012aa5
Arpita Saha <arpita.sahaa@gmail.com>
2020-11-19 19:02:11 -0500

    added questions to seperate js file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require("inquirer");
-var mysql = require("mysql");
+const mysql = require("mysql");
+const mainMenuQues = require("./questions");
 
-//Define database connection here
+// Define database connection here
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -10,9 +11,53 @@ const connection = mysql.createConnection({
   database: "walmart",
 });
 
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  init();
-});
+// connection.connect(function (err) {
+//   if (err) throw err;
+//   console.log("connected as id " + connection.threadId + "\n");
+//   init();
+// });
 
+function init() {
+  inquirer.prompt(mainMenuQues).then((response) => {
+    console.log(response);
+  });
+  
+}
+init();
+
+// function departments(){
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             message: "what department you would like to add? ",
+//             name: "deptName",
+//         }
+//     ])
+//     .then((response)=>{
+//         console.log(response);
+//         insertToDataBase(response);
+//     });
+// }
+
+// function insertToDataBase(response) {
+//     var query = connection.query(
+//       "INSERT INTO department SET ?",
+//       {
+//         dept_name: response.deptName,
+
+//       },
+//       function (err, res) {
+//         if (err) throw err;
+//         console.log(res.affectedRows + " product inserted!\n");
+//         init();
+//       }
+//     );
+//   }
+
+//   function showDepartments(){
+//     connection.query("select * from department", function (err, res) {
+//         if (err) throw err;
+//         console.log(res);
+//     })
+
+//   }
